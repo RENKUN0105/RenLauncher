7ded58721d842f385a40ae3bea53cf63a2925d1d
RENKUN0105 <115170935+RENKUN0105@users.noreply.github.com>
2024-07-13 01:20:26 +0900

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://5c3dc8a5-cae7-4f91-8426-6ba2ef115b9d-00-2tmoymzccwwag.sisko.replit.dev/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://5c3dc8a5-cae7-4f91-8426-6ba2ef115b9d-00-2tmoymzccwwag.sisko.replit.dev/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
